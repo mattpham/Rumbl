728656a3c7fa7b02f92670d8d4ca5f577a50d467
matt pham <matt@dreymur.com>
2018-09-14 02:27:20 -0700

    Change accounts context to use Ecto

--- lib/rumbl/accounts/accounts.ex
+++ lib/rumbl/accounts/accounts.ex
@@ -5,28 +5,33 @@ defmodule Rumbl.Accounts do
 
   alias Rumbl.Accounts.User
 
-  # !Temporarily hardcode Users
-  def list_users do
-    [
-      %User{id: "1", name: "Jose", username: "josevalim"},
-      %User{id: "2", name: "Bruce", username: "redrapids"},
-      %User{id: "3", name: "Chris", username: "chrismccord"},
-    ]
-  end
+  alias Rumbl.Repo
 
   @doc """
   Returns a single user with matching id.
   """
   def get_user(id) do
-    Enum.find(list_users(), fn map -> map.id == id end)
+    Repo.get(User, id)
+  end
+
+  @doc """
+  Raises an `Ecto.NotFoundError` if user does not exist.
+  """
+  def get_user!(id) do
+    Repo.get!(User, id)
   end
 
   @doc """
   Returns a single user matching a single attribute or list of attributes.
   """
   def get_user_by(params) do
-    Enum.find(list_users(), fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end)
+    Repo.get_by(User, params)
+  end
+
+  @doc """
+  Returns a list of all users.
+  """
+  def list_users do
+    Repo.all(User)
   end
 end
