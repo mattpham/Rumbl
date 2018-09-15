6a4aab4c297ca4baf9967fa882e700f9f8e77ca3
matt pham <matt@dreymur.com>
2018-09-15 16:20:50 -0700

    Add password hashing in credential and fix typo in credentials migration

--- priv/repo/migrations/20180915223828_create_credentials.exs
+++ priv/repo/migrations/20180915223828_create_credentials.exs
@@ -3,9 +3,9 @@ defmodule Rumbl.Repo.Migrations.CreateCredentials do
 
   def change do
     create table(:credentials) do
-      add :email, :string, null:false
+      add :email, :string, null: false
       add :password_hash, :string, null: false
-      add :user_id, references(:user, on_delete: :delete_all, null: false)
+      add :user_id, references(:users, on_delete: :delete_all, null: false)
 
       timestamps()
     end
