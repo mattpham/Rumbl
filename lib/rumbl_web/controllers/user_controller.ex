246c581675d3a8e7ee71e0e3e69030fe769f6667
matt pham <matt@dreymur.com>
2018-09-16 14:52:16 -0700

    Add multimedia and video context

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -3,7 +3,7 @@ defmodule RumblWeb.UserController do
 
   alias Rumbl.Accounts
 
-  plug :authenticate when action in [:index, :show]
+  plug :authenticate_user when action in [:index, :show]
 
   def index(conn, _params) do
     users = Accounts.list_users()
@@ -33,22 +33,6 @@ defmodule RumblWeb.UserController do
       {:error, %Ecto.Changeset{} = changeset} ->
         conn
         |> render("new.html", changeset: changeset)
-    end
-
-
-  end
-
-  ###
-  #  Authenticate plug for controller pipeline
-  ###
-  defp authenticate(conn, _opts) do
-    if conn.assigns.current_user do
-      conn
-    else
-      conn
-      |> put_flash(:error, "You must be logged in to access that page")
-      |> redirect(to: Routes.page_path(conn, :index))
-      |> halt()
     end
   end
 end
