fb6bd231a450461b79fdb6600d52d236acae6170
matt pham <matt@dreymur.com>
2018-09-25 14:44:24 -0700

    Add socket authentication

--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -14,10 +14,10 @@ defmodule RumblWeb.Auth do
       # Improving the contract, if user already in conn.assigns, honor
       #   the request.
       user = conn.assigns[:current_user] ->
-        conn
+        put_current_user(conn, user)
 
       user = user_id && Accounts.get_user(user_id) ->
-        assign(conn, :current_user, user)
+        put_current_user(conn, user)
 
       true ->
         assign(conn, :current_user, nil)
@@ -29,6 +29,16 @@ defmodule RumblWeb.Auth do
     |> assign(:current_user, user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
+  end
+
+  # places a freshly generated user token and the current_user into conn.assigns which is used in
+  # the call function
+  defp put_current_user(conn, user) do
+    token = Phoenix.Token.sign(conn, "user socket", user.id)
+
+    conn
+    |> assign(:current_user, user)
+    |> assign(:user_token, token)
   end
 
   def login_by_email_and_pass(conn, email, given_pass) do
