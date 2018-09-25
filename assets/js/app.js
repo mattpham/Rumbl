3efe9b1fec6177daccf483ede4fcf5550c7e4a9e
matt pham <matt@dreymur.com>
2018-09-25 13:20:57 -0700

    Add client side video object to handle video sockets
    changed app.js to initialize video object instead of player

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import css from "../css/app.css"
+import css from '../css/app.css';
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -9,20 +9,15 @@ import css from "../css/app.css"
 //
 // Import dependencies
 //
-import "phoenix_html"
+import 'phoenix_html';
 
 // Import local files
 //
 // Local files can be imported directly using relative
 // paths "./socket" or full ones "web/static/js/socket".
 
-// import socket from "./socket"
+import socket from './socket';
 
-import Player from "./player"
-let video = document.getElementById("video")
+import Video from './video';
 
-if(video){
-  Player.init(video.id, video.getAttribute("data-player-id"), () => {
-    console.log("player ready!")
-  })
-}
+Video.init(socket, document.getElementById('video'));
