e8a9196d076231791305566774eb2333315659b4
matt pham <matt@dreymur.com>
2018-10-20 14:29:25 -0700

    Change implementation of Counter to GenServer

--- lib/rumbl/counter.ex
+++ lib/rumbl/counter.ex
@@ -1,36 +1,24 @@
 defmodule Rumbl.Counter do
-@moduledoc """
-  Module that implements a Counter server as well as functions for itneracting with it as a client.
+  @moduledoc """
+    Module that implements a Counter server as well as functions for itneracting with it as a client.
 
-  The `client` serves as the API and exists only to send messages to the process that does the work.
-  It's the `interface` for the counter. The `server` is a process that recursively loops, processing
-  a message and sending updated state to itself. Our server is the `implementation`.
-"""
+    The `client` serves as the API and exists only to send messages to the process that does the work.
+    It's the `interface` for the counter. The `server` is a process that recursively loops, processing
+    a message and sending updated state to itself. Our server is the `implementation`.
+  """
+
+  use GenServer
+
   ##
   # Client API
   ##
 
-  def inc(pid), do: send(pid, :inc)
+  # GenServer.cast does not return a value
+  def inc(pid), do: GenServer.cast(pid, :inc)
 
-  def dec(pid), do: send(pid, :dec)
-
-  @doc """
-    Function that sends a request for a value of the coutner and must await the response.
-
-  """
-  def val(pid, timeout \\ 5000) do
-      # make_ref() creates a unique reference, thats just a value guaranteed to be globally unique,
-      # to associate with the particular request
-      ref = make_ref()
-      send(pid, {:val, self(), ref})
-
-      # Awaits for the response to match on the reference.
-      receive do
-        {^ref, val} -> val # ^ref means it is matching prev ref vs rebinding.
-      after
-        timeout -> exit(:timeout)
-      end
-  end
+  def dec(pid), do: GenServer.cast(pid, :dec)
+  # GenServer.call with return the state of the server
+  def val(pid), do: GenServer.call(pid, :val)
 
   ###
   # Server Implementation
@@ -39,22 +27,24 @@ defmodule Rumbl.Counter do
   # `start_link` function required by OTP. Accepts only initial state, and its job is to spawn a
   # process and return {:ok, pid}, where pid it the identifier of the spawned process
   def start_link(initial_val) do
-    {:ok, spawn_link(fn -> listen(initial_val) end)}
+    GenServer.start_link(__MODULE__, initial_val)
   end
 
-  # Function that maintains state through recursive calls.
-  # Each call to listen waits for a message and does the matching action before callint itself again.
-  defp listen(val) do
-    receive do
-      :inc ->
-        listen(val + 1)
+  def init(initial_val) do
+    {:ok, initial_val}
+  end
 
-      :dec ->
-        listen(val - 1)
+  def handle_cast(:inc, val) do
+    {:noreply, val + 1}
+  end
 
-      {:val, sender, ref} ->
-        send(sender, {ref, val})
-        listen(val)
-    end
+  def handle_cast(:dec, val) do
+    {:noreply, val - 1}
+  end
+
+  # the arugument with a leading `_` is used to explicitely describe the argument while ignoreing
+  # the contents
+  def handle_call(:val, _from, val) do
+    {:reply, val, val}
   end
 end
